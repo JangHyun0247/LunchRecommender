4e97c5beba55b72a81603410b83fcc1698501c1b
JangHyun <yugi82828@naver.com>
2024-06-13 21:55:10 +0900

    # test : PostTest (테스트 코드 추가)

---
+++ src/test/java/com/sparta/lunchrecommender/entity/PostTest.java
@@ -0,0 +1,34 @@
+package com.sparta.lunchrecommender.entity;
+
+import com.sparta.lunchrecommender.domain.post.dto.PostCreateRequestDto;
+import com.sparta.lunchrecommender.domain.post.dto.PostUpdateRequestDto;
+import com.sparta.lunchrecommender.domain.post.entity.Post;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Nested
+@DisplayName("게시물 엔티티 테스트")
+class PostTest {
+
+    PostCreateRequestDto postCreate = new PostCreateRequestDto("게시물 내용");
+    private final Post post = new Post(postCreate);
+
+    @Test
+    @DisplayName("게시물 업데이트 테스트")
+    void update() {
+
+        //given
+        PostUpdateRequestDto postUpdate = new PostUpdateRequestDto("게시물 내용 수정");
+
+        //when
+        post.update(postUpdate);
+
+        //then
+        assertNotEquals("게시물 내용",post.getContent());
+        assertEquals("게시물 내용 수정",post.getContent());
+
+    }
+}
